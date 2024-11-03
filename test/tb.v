db62b6df93defbe2a3073f22904a545fbee7bc43
rej <rzioma@acm.org>
2024-11-04 00:06:39 +0100

    iHP0p2: updated files for iHP process

--- test/tb.v
+++ test/tb.v
@@ -23,15 +23,7 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
   tt_um_rejunity_vga_logo user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
-
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
